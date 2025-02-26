70cafc2ea6eb6ef73a3994c083c2a9e08c2bad3a
klmr ✌🏼 <105491169+MaxRichet@users.noreply.github.com>
2025-02-26 11:13:33 +0100

    ep1 done

---
+++ s2/ep1/src/generics.js
@@ -0,0 +1,10 @@
+function premierElement(tableau) {
+    console.log(tableau[0]);
+    return tableau[0];
+}
+var number = [1, 2, 3, 4, 5];
+var string = ["str1", "str2", "str3", "str4", "str5"];
+var obj = [{ nom: "Max", age: 22 }, { nom: "William", age: 25 }, { nom: "Maud", age: 20 }];
+premierElement(number);
+premierElement(string);
+premierElement(obj);
