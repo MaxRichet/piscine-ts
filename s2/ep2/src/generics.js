4550f3ccfb18984d80e5728947569b96e8806a90
klmr ✌🏼 <105491169+MaxRichet@users.noreply.github.com>
2025-02-26 14:53:45 +0100

    fix ep2

--- s2/ep2/src/generics.js
+++ s2/ep2/src/generics.js
@@ -3,14 +3,15 @@ function premierElement() {
     for (var _i = 0; _i < arguments.length; _i++) {
         tableau[_i] = arguments[_i];
     }
-    console.log(tableau[0], "test");
+    if (tableau.length <= 0)
+        return undefined;
     return tableau[0];
 }
 var number = [1, 2, 3, 4, 5];
 var string = ["str1", "str2", "str3", "str4", "str5"];
 var obj = [{ nom: "Max", age: 22 }, { nom: "William", age: 25 }, { nom: "Maud", age: 20 }];
-var stringVide = [];
+var arrayVide = [];
 premierElement(number);
 premierElement(string);
 premierElement(obj);
-premierElement(stringVide);
+premierElement(arrayVide);
--- s2/ep2/src/generics.ts
+++ s2/ep2/src/generics.ts
@@ -1,14 +1,16 @@
-function premierElement<T extends any[]>(...tableau: T): T{
-    console.log(tableau[0], "test");
+function premierElement<T extends any[]>(...tableau: T): T | undefined{
+    if(tableau.length <= 0) return undefined;
     return tableau[0];
 }
+
 type Obj = {nom: string, age: number};
+
 const number: number[] = [1, 2, 3, 4, 5];
 const string: string[] = ["str1", "str2", "str3", "str4", "str5"];
 const obj: Obj[] = [{nom: "Max", age: 22}, {nom: "William", age: 25}, {nom: "Maud", age: 20}];
-const stringVide: string[] = [];
+const arrayVide: string[] = [];
 
 premierElement(number);
 premierElement(string);
 premierElement(obj);
-premierElement(stringVide);
+premierElement(arrayVide);
