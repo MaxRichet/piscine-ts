259c8192cbd525784cf5162e7b21acbe90f04484
klmr ✌🏼 <105491169+MaxRichet@users.noreply.github.com>
2025-02-26 12:17:42 +0100

    ep2 done

---
+++ s2/ep2/src/generics.js
@@ -0,0 +1,16 @@
+function premierElement() {
+    var tableau = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        tableau[_i] = arguments[_i];
+    }
+    console.log(tableau[0]);
+    return tableau[0];
+}
+var number = [1, 2, 3, 4, 5];
+var string = ["str1", "str2", "str3", "str4", "str5"];
+var obj = [{ nom: "Max", age: 22 }, { nom: "William", age: 25 }, { nom: "Maud", age: 20 }];
+var stringVide = [];
+premierElement(number);
+premierElement(string);
+premierElement(obj);
+premierElement(stringVide);
---
+++ s2/ep2/src/generics.ts
@@ -0,0 +1,14 @@
+function premierElement<T extends any[]>(...tableau: T): T{
+    console.log(tableau[0]);
+    return tableau[0];
+}
+type Obj = {nom: string, age: number};
+const number: number[] = [1, 2, 3, 4, 5];
+const string: string[] = ["str1", "str2", "str3", "str4", "str5"];
+const obj: Obj[] = [{nom: "Max", age: 22}, {nom: "William", age: 25}, {nom: "Maud", age: 20}];
+const stringVide: string[] = [];
+
+premierElement(number);
+premierElement(string);
+premierElement(obj);
+premierElement(stringVide);
